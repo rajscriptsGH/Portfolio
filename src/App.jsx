bc50abaf99a22fa6072bb832830e131c6cc10063
rajyadav63 <vector36razz@gmail.com>
2025-06-17 00:38:19 +0530

    tagline added

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className='h-screen w-screen flex justify-center items-center flex-col bg-gray-100'>
+      <p className='absolute top-5 bg-green-400 text-white px-4 py-1 rounded-lg text-center'>Rvmp soon</p>
       <div className='mt-[-150px]'>
         <img className='h-[200px] rounded-2xl shadow-lg' src="/profile.jpg" alt="Profile" />
       </div>
 
       <div className='flex flex-col justify-center items-center mt-4'>
         <h1 className='text-2xl font-semibold mb-1'>Vector</h1>
-        <h1 className='font-semibold mb-1'>Full STUCK Developer</h1>
+        <p className='mb-2'>Full STUCK Developer</p>
 
         <div className='flex gap-3 mb-4'>
           <a href="https://x.com/rajscripts100x" target='_blank' rel="noopener noreferrer">
             <img className='h-[40px]' src="/x.png" alt="Twitter/X" />
           </a>
-          <a href="https://x.com/rajyadav63" target='_blank' rel="noopener noreferrer">
+          <a href="https://github.com/rajyadav63" target='_blank' rel="noopener noreferrer">
             <img className='h-[40px]' src="/github.png" alt="github" />
           </a>
           <a href="https://hashnode.com/@yadavraj36" target='_blank' rel="noopener noreferrer">
