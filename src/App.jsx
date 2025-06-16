5cde0cc1f96e9e9710cbc1c61c71ecd5ecd6751f
rajyadav63 <vector36razz@gmail.com>
2025-06-16 22:32:52 +0530

    Portfolio Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className='h-screen w-screen flex justify-center items-center flex-col bg-gray-100'>
-      {/* Profile Image */}
       <div className='mt-[-150px]'>
         <img className='h-[200px] rounded-2xl shadow-lg' src="/profile.jpg" alt="Profile" />
       </div>
 
       <div className='flex flex-col justify-center items-center mt-4'>
-        <h1 className='text-2xl font-semibold mb-4'>Vector</h1>
+        <h1 className='text-2xl font-semibold mb-1'>Vector</h1>
+        <h1 className='font-semibold mb-1'>Full STUCK Developer</h1>
 
         <div className='flex gap-3 mb-4'>
           <a href="https://x.com/rajscripts100x" target='_blank' rel="noopener noreferrer">
@@ -21,17 +21,15 @@ function App() {
           <a href="https://hashnode.com/@yadavraj36" target='_blank' rel="noopener noreferrer">
             <img className='h-[40px]' src="/hashnode.png" alt="hashnode" />
           </a>
+          <a href="https://linkedin.com/in/rajscripts100in/" target='_blank' rel="noopener noreferrer">
+            <img className='h-[40px]' src="/linkedin.png" alt="linkedin" />
+          </a>
         </div>
 
         <div className='flex justify-center items-center text-center sm:w-[550px] w-[320px] px-2'>
           <p>
-            I write <span>code</span>, <span>break</span> things, then <span>fix</span> them, usually in that order. Find me on best Web above. Pretending to be productive.
+            I write <span>code</span>, <span>break</span> things, then <span>fix</span> them, usually in that order. Pretending to be productive.
           </p>
-        </div>
-        <div className='flex gap-3 mt-4'>
-          <p className='px-4 py-1 rounded-lg bg-red-400 outline-none'>HTML</p>
-          <p className='px-4 py-1 rounded-lg bg-blue-400 outline-none'>CSS</p>
-          <p className='px-4 py-1 rounded-lg bg-yellow-400 outline-none'>JS</p>
         </div>
       </div>
     </div>
