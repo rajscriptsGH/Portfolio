4cafa8441c558972df530087d0897dbad35b9cbc
rajyadav63 <vector36razz@gmail.com>
2025-06-17 01:09:34 +0530

    bottom tagline added

--- src/App.jsx
+++ src/App.jsx
@@ -28,9 +28,13 @@ function App() {
           </a>
         </div>
 
-        <div className='flex justify-center items-center text-center sm:w-[550px] w-[320px] px-2'>
+        <div className='flex flex-col justify-center items-center text-center sm:w-[550px] w-[320px] px-2'>
           <p>
             I write <span>code</span>, <span>break</span> things, then <span>fix</span> them, usually in that order. Pretending to be productive.
+          </p>
+          <p className='flex justify-center items-center text-center underline text-blue-400 gap-0 '>
+            <img className='h-9' src="/dot.png" alt="dot" />
+            World best website
           </p>
         </div>
       </div>
