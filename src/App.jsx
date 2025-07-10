7d3c769904150087c7f2b30cad9e7840465f44d7
rajyadav63 <vector36razz@gmail.com>
2025-07-10 08:24:43 +0530

    cto removed

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Social from './components/Social';
 function App() {
   return (
     <div className='h-screen w-screen flex justify-center items-center flex-col bg-gray-100'>
-      <p className='absolute top-5 bg-green-400 text-white px-4 py-1 rounded-lg text-center'>Rvmp soon...</p>
       <Profile />
       <Social />
       <Description />
