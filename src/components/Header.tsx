d9d6f84fef283bb593e606302f748e2759961b90
rajyadav63 <vector36razz@gmail.com>
2025-08-27 21:03:23 +0530

    header added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,28 @@
+'use client'
 import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
     return (
-        <div className='flex flex-col justify-center items-center mt-12'>
+        <div className='flex flex-col justify-center items-center mt-15'>
             <div>
                 <Image
                     src="/profile.jpg"
                     alt='profile_pic'
-                    width={140}
+                    width={190}
                     height={140}
-                    className='rounded-full'
+                    className='rounded-4xl'
                 />
             </div>
-            <div className="flex flex-col items-center mt-4 text-center space-y-3">
+            <div className="flex flex-col items-center mt-5 text-center space-y-3">
                 <p className="text-lg">👋 Hi, I'm <span className="font-semibold">Razz</span></p>
                 <h1 className="text-3xl md:text-5xl font-bold">
                     Full Stack Developer
                 </h1>
-                <p className="text-gray-300 max-w-xl">
-                    I create scalable apps and intuitive interfaces by combining design thinking with technical expertise
+                <p className="text-gray-300 max-w-xl w-fit">
+                    I create scalable apps and intuitive interfaces by combining design with technical expertise
                 </p>
             </div>
-
-            <div>
-                <button>Contact</button>
-                <button>Resume</button>
-            </div>
-
         </div>
     )
 }
