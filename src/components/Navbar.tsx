b62659587862e67b39d24c076805891d95d3267b
rajyadav63 <vector36razz@gmail.com>
2025-08-27 20:14:56 +0530

    navbar completed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,49 @@
+'use client'
 import React from 'react'
 import Image from "next/image";
 
 const Navbar = () => {
     return (
         <>
-            <nav className='flex justify-around items-center w-full fixed top-5 px-5 lg-px-8 py-4'>
-                <a
-                    className='text-3xl font-medium'
-                    href="/">
-                    Portfolio.
-                </a>
+            <div className='absolute top-0 left-0 w-full h-16 -z-10'>
+                <Image
+                    src="/header-bg-color.png"
+                    alt="header"
+                    fill
+                    className="object-cover"
+                />
 
-                <ul className='border  px-3 lg:px-12 py-2 text-center rounded-2xl flex '>
-                    <li className='px-2 hover:text-slate-600'><a href="#home">Home</a></li>
-                    <li className='px-2 hover:text-slate-600'><a href="#work">Works</a></li>
-                    <li className='px-2 hover:text-slate-600'><a href="#contact">Contact</a></li>
+            </div>
+            <nav className='flex justify-between md:justify-around items-center w-full py-3 px-16 z-50'>
+                <div>
+                    <a
+                        className='text-2xl md:text-3xl text-black font-semibold'
+                        href="/">
+                        Portfolio.
+                    </a>
+                </div>
+
+                <ul className='hidden md:flex items-center py-2 rounded-full px-4 gap-2 lg:gap-5 bg-slate-600/90 shadow-sm'>
+                    <li>
+                        <a className='px-2 hover:text-slate-300 font-sans' href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a className='px-2 hover:text-slate-300 font-sans' href="#work">
+                            Project
+                        </a>
+                    </li>
+                    <li>
+                        <a className='px-2 hover:text-slate-300 font-sans' href="#contact">Contact</a>
+                    </li>
                 </ul>
-                <a href="#contact">
-                    <Image
-                        className='w-8'
-                        src="/moon_icon.png"
-                        alt='mode'
-                        width={30} height={30} />
-                </a>
+                <div>
+                    <button>
+                        <Image
+                            src="/moon_icon.png"
+                            alt='mode'
+                            width={23} height={23} />
+                    </button>
+                </div>
             </nav>
         </>
     )
