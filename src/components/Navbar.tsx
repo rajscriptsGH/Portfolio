0003538431f9f67e1e15377c237e035bb25e0599
rajyadav63 <vector36razz@gmail.com>
2025-08-27 18:17:33 +0530

    assets added in public

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Image from "next/image";
 
 const Navbar = () => {
     return (
-        <div>
+        <>
+            <nav>
+                <a href="/">Portfolio.</a>
 
-        </div>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#work">Works</a></li>
+                    <li><a href="#skill">Skill</a></li>
+                    <li><a href="#service">Service</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+                <a href="#contact">
+                    <Image src="/moon_icon" alt='mode' />
+                </a>
+            </nav>
+        </>
     )
 }
 
