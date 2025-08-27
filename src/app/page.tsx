387e7a921bf673d5837d5067455a7d331e4f0461
rajyadav63 <vector36razz@gmail.com>
2025-08-27 20:15:14 +0530

    navbar done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="bg-slate-700 flex flex-col justify-center items-center w-full">
+    <div>
       <Navbar />
     </div>
   );
