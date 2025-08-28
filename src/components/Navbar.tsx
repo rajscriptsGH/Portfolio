ce772c0c8e458817f9bcaf513bf4fd525a5f3c3e
rajyadav63 <vector36razz@gmail.com>
2025-08-28 20:29:58 +0530

    navbar postitin changed from fixed to relative

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,9 @@ import Image from "next/image";
 
 const Navbar = () => {
     return (
-        <div className='fixed top-0 left-0 w-full h-16 z-50'>
+        <div>
 
-            {/* <Image
-                src="/header-bg-color.png"
-                alt="header"
-                fill
-                className="object-cover -z-10"
-            /> */}
-
-
-            <nav className='flex justify-between md:justify-around items-center w-full py-3 px-16 relative z-10 mt-12'>
+            <nav className='flex justify-center items-center w-full py-3 px-16 relative z-10 mt-20'>
                 <ul className='flex items-center py-3 rounded-lg px-4 gap-2 lg:gap-10 bg-slate-600/90 shadow-sm'>
                     <li>
                         <a className='px-2 hover:text-slate-300 font-sans' href="#home">Home</a>
