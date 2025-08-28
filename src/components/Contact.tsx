a96dfa79410060688b45051cc59d0baa54fb69ed
rajyadav63 <vector36razz@gmail.com>
2025-08-28 17:03:37 +0530

    id added to contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div className="flex flex-col items-center justify-center py-10 px-4 mt-10">
+        <div id='contact' className="flex flex-col items-center justify-center py-10 px-4 mt-10">
             <h2 className="text-3xl font-bold mb-6">Contact Me</h2>
             <p className="text-gray-400 mb-8 text-center max-w-xl">
                 Schedule a 30-minute call with me directly through my calendar.
