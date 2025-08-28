676d74559e346389ac3a88bb7cab8dae45a54c06
rajyadav63 <vector36razz@gmail.com>
2025-08-28 13:03:59 +0530

    about section done

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,30 +4,17 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='mt-15 flex flex-col justify-center items-center'>
-      <h1 className='text-3xl text-center font-semibold font-Ovo'>About</h1>
+    <div className='mt-15 px-5 lg:ml-25'>
+      <h1 className='text-2xl font-semibold font-Ovo'>About Me</h1>
 
-      <div className='flex flex-col md:flex-row gap-10 text-center items-center justify-center mt-10'>
-        <div className="relative w-40 h-40 md:w-60 md:h-60 lg:w-80 lg:h-80">
-          <Image
-            src="/profile.jpg"
-            alt="profile_pic"
-            fill
-            className="object-cover rounded-3xl"
-          />
-        </div>
+      <div className='flex flex-col  md:flex-row gap-2 items-center justify-center mt-4 max-w-[600px]'>
+        <p className=''>Hi! I'm <span className='font-bold'>Raj Yadav</span>, a Computer Science graduate and Full-Stack Developer passionate about building impactful digital products.
+        </p>
 
-        <div className='max-w-[300px] md:max-w-[400px] text-center'>
-          <p>Full Stack Developer and student passionate about turning ideas into functional products. I enjoy solving problems, creating user-friendly experiences, and building solutions with real-world impact.
-          </p>
-          <div className='flex gap-10 mt-5'>
-            <p>1</p>
-            <p>1</p>
-            <p>1</p>
-            <p>1</p>
-            <p>1</p>
-          </div>
-        </div>
+        <p> I specialize in MERN stack, Next.js, and TypeScript, and I'm currently diving into Web3 while exploring UI/UX design.</p>
+
+        <p>I love creating user-friendly applications, contributing to open-source projects, and solving real-world problems through code.</p>
+
       </div>
     </div>
   )
