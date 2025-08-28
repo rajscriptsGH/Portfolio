d513e1f2d3994bd84dd83951584113a1987fb5a7
rajyadav63 <vector36razz@gmail.com>
2025-08-28 20:14:28 +0530

    nav bg removed and updated it position

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,24 +6,16 @@ const Navbar = () => {
     return (
         <div className='fixed top-0 left-0 w-full h-16 z-50'>
 
-            <Image
+            {/* <Image
                 src="/header-bg-color.png"
                 alt="header"
                 fill
                 className="object-cover -z-10"
-            />
+            /> */}
 
 
-            <nav className='flex justify-between md:justify-around items-center w-full py-3 px-16 relative z-10'>
-                <div>
-                    <a
-                        className='text-2xl md:text-3xl text-black font-semibold'
-                        href="/">
-                        Portfolio.
-                    </a>
-                </div>
-
-                <ul className='flex items-center py-2 rounded-full px-4 gap-2 lg:gap-10 bg-slate-600/90 shadow-sm'>
+            <nav className='flex justify-between md:justify-around items-center w-full py-3 px-16 relative z-10 mt-12'>
+                <ul className='flex items-center py-3 rounded-lg px-4 gap-2 lg:gap-10 bg-slate-600/90 shadow-sm'>
                     <li>
                         <a className='px-2 hover:text-slate-300 font-sans' href="#home">Home</a>
                     </li>
