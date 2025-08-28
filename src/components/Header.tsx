c21da7b8e8d14450b1b678e00cc65bbeded4dda0
rajyadav63 <vector36razz@gmail.com>
2025-08-28 12:21:45 +0530

    header optimized

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,24 +4,54 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className='flex flex-col justify-center items-center mt-24'>
-            <div>
+        <div className='flex justify-start items-center mt-30 lg:ml-32 gap-4'>
+            <div className="relative w-40 h-40 md:w-50 md:h-45">
                 <Image
                     src="/profile.jpg"
-                    alt='profile_pic'
-                    width={160}
-                    height={140}
-                    className='rounded-4xl'
+                    alt="profile_pic"
+                    fill
+                    className="object-cover rounded-3xl"
                 />
             </div>
-            <div className="flex flex-col items-center mt-5 text-center space-y-3">
-                <p className="text-lg">👋 Hi, I'm <span className="font-semibold">Razz</span></p>
-                <h1 className="text-3xl md:text-5xl font-bold">
-                    Full Stack Developer
-                </h1>
-                <p className="text-gray-300 max-w-xl w-fit">
-                    I create scalable apps and intuitive interfaces by combining design with technical expertise
-                </p>
+            <div className="flex flex-col">
+                <h1 className='text-4xl font-sans font-bold'>Raj Yadav</h1>
+                <p className='font-sans font-light'>Software Engineer</p>
+                <div className='flex gap-3 mt-2'>
+                    <a href="https://github.com/rajscriptsGH" target="_blank" className='relative group'>
+                        <div className="p-2 border border-slate-500 rounded-md hover:border-slate-200 transition">
+                            <Image
+                                src="/github.png"
+                                alt="github"
+                                width={24} height={24} />
+                        </div>
+                        <span className="absolute -top-8 left-1/2 -translate-x-1/2 bg-slate-200 text-black text-sm px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
+                            GitHub
+                        </span>
+                    </a>
+                    <a href="https://x.com/rajscriptsX" target="_blank" className='relative group'>
+                        <div className="p-2 border border-slate-500 rounded-md hover:border-slate-200 transition">
+                            <Image
+                                src="/x.png"
+                                alt="github"
+                                width={24} height={24} />
+                        </div>
+                        <span className="absolute -top-8 left-1/2 -translate-x-1/2 bg-slate-200 text-black text-sm px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
+                            X
+                        </span>
+                    </a>
+                    <a href="https://linkedin.com/rajscriptsin" target="_blank" className='relative group'>
+                        <div className="p-2 border border-slate-500 rounded-md hover:border-slate-200 transition">
+                            <Image
+                                src="/linkedin.png"
+                                alt="github"
+                                width={24} height={24} />
+                        </div>
+                        <span className="absolute -top-8 left-1/2 -translate-x-1/2 bg-slate-200 text-black text-sm px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
+                            Linkedin
+                        </span>
+                    </a>
+
+                </div>
             </div>
         </div>
     )
