79cc15a63ea024b4b090089ceee9b45b9c325e9f
rajyadav63 <vector36razz@gmail.com>
2025-08-28 15:01:04 +0530

    final update before adding projects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`${outfit.variable} ${ovo.variable} antialiased bg-slate-800`}
+        className={`${outfit.variable} ${ovo.variable} antialiased bg-slate-900`}
       >
-        <div className="max-w-6xl mx-auto px-8 md:px-5">
+        <div className="max-w-6xl mx-auto px-6 md:px-5">
           {children}
         </div>
       </body>
