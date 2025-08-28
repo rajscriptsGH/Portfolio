989280fe0e70de355a31a30e8fc7be643fc08ff3
rajyadav63 <vector36razz@gmail.com>
2025-08-28 12:21:07 +0530

    github updated

--- src/components/Github.tsx
+++ src/components/Github.tsx
@@ -4,7 +4,7 @@ import GitHubCalendar from 'react-github-calendar';
 
 const Github = () => {
     return (
-        <div className='flex justify-center items-center mt-10 px-5'>
+        <div className='flex justify-center items-center mt-10 px-2'>
             <div className='border border-slate-500 p-4 rounded-md overflow-hidden w-full max-w-[900px]'>
                 <div className='scale-98 md:scale-100 origin-top-left'>
                     <GitHubCalendar username="rajscriptsGH" />
