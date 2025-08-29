63341aaeed876495eac2ca81498bae8e67b07983
rajyadav63 <vector36razz@gmail.com>
2025-08-29 17:33:03 +0530

    header updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
                 />
             </div>
             <div className="flex flex-col">
-                <h1 className='text-2xl sm:text-4xl font-sans font-bold'>Raj Yadav
+                <h1 className='text-2xl sm:text-4xl font-sans font-bold'>Razz
                     <span className='relative top-[-15px] right-[-10px] text-[15px]  text-green-500 border px-[10px] py-[5px] rounded-md'>Available</span>
                 </h1>
                 <p className='font-sans font-light'>Software Engineer</p>
