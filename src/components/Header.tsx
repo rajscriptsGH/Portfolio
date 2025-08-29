49f69b316ec7fa52f28f634c315f2c184284ef32
rajyadav63 <vector36razz@gmail.com>
2025-08-29 17:44:32 +0530

    updated header for responisiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,12 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div id='home' className='flex justify-start items-center mt-30 lg:ml-32 md:ml-[25px] gap-4'>
-            <div className="relative w-40 h-40 md:w-50 md:h-45">
+        <div
+            id="home"
+            className="flex flex-col items-center justify-center sm:flex-row sm:items-start sm:justify-start mt-10 sm:mt-10 lg:mt-20 lg:ml-32 md:ml-[25px] gap-10 sm:gap-6"
+        >
+            {/* Profile Picture */}
+            <div className="relative w-40 h-40 md:w-50 md:h-45 flex-shrink-0">
                 <Image
                     src="/profile.jpg"
                     alt="profile_pic"
@@ -13,68 +17,63 @@ const Header = () => {
                     className="object-cover rounded-3xl"
                 />
             </div>
-            <div className="flex flex-col">
-                <h1 className='text-2xl sm:text-4xl font-sans font-bold'>Razz
-                    <span className='relative top-[-15px] right-[-10px] text-[15px]  text-green-500 border px-[10px] py-[5px] rounded-md'>Available</span>
+
+            {/* Text Section */}
+            <div className="flex flex-col text-center sm:text-left">
+                <h1 className="text-2xl sm:text-4xl font-sans font-bold">
+                    Razz
+                    <span className="relative top-[-15px] right-[-10px] text-[15px] text-green-500 border px-[10px] py-[5px] rounded-md">
+                        Available
+                    </span>
                 </h1>
-                <p className='font-sans font-light'>Software Engineer</p>
-                <div className='flex gap-3 mt-2'>
-                    <a href="https://github.com/rajscriptsGH" target="_blank" className='relative group'>
+                <p className="font-sans font-light">Software Engineer</p>
+
+                {/* Social Links */}
+                <div className="flex justify-center sm:justify-start gap-3 mt-2">
+                    <a href="https://github.com/rajscriptsGH" target="_blank" className="relative group">
                         <div className="p-2 border border-slate-500 rounded-md hover:border-slate-200 transition">
-                            <Image
-                                src="/github.png"
-                                alt="github"
-                                width={24} height={24} />
+                            <Image src="/github.png" alt="github" width={24} height={24} />
                         </div>
                         <span className="absolute -top-8 left-1/2 -translate-x-1/2 bg-slate-200 text-black text-sm px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
                             GitHub
                         </span>
                     </a>
-                    <a href="https://x.com/rajscriptsX" target="_blank" className='relative group'>
+
+                    <a href="https://x.com/rajscriptsX" target="_blank" className="relative group">
                         <div className="p-2 border border-slate-500 rounded-md hover:border-slate-200 transition">
-                            <Image
-                                src="/x.png"
-                                alt="github"
-                                width={24} height={24} />
+                            <Image src="/x.png" alt="x" width={24} height={24} />
                         </div>
                         <span className="absolute -top-8 left-1/2 -translate-x-1/2 bg-slate-200 text-black text-sm px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
                             X
                         </span>
                     </a>
-                    <a href="https://linkedin.com/rajscriptsin" target="_blank" className='relative group'>
+
+                    <a href="https://linkedin.com/rajscriptsin" target="_blank" className="relative group">
                         <div className="p-2 border border-slate-500 rounded-md hover:border-slate-200 transition">
-                            <Image
-                                src="/linkedin.png"
-                                alt="github"
-                                width={24} height={24} />
+                            <Image src="/linkedin.png" alt="linkedin" width={24} height={24} />
                         </div>
                         <span className="absolute -top-8 left-1/2 -translate-x-1/2 bg-slate-200 text-black text-sm px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
                             Linkedin
                         </span>
                     </a>
-                    <a href="mailto:vector36razz@gmail.com" target="_blank" className='relative group'>
+
+                    <a href="mailto:vector36razz@gmail.com" target="_blank" className="relative group">
                         <div className="p-2 border border-slate-500 rounded-md hover:border-slate-200 transition">
-                            <Image
-                                src="/gmail.png"
-                                alt="github"
-                                width={24} height={24} />
+                            <Image src="/gmail.png" alt="gmail" width={24} height={24} />
                         </div>
                         <span className="absolute -top-8 left-1/2 -translate-x-1/2 bg-slate-200 text-black text-sm px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
                             Gmail
                         </span>
                     </a>
-                    <a href="https://wa.me/+9779863452890" target="_blank" className='relative group'>
+
+                    <a href="https://wa.me/+9779863452890" target="_blank" className="relative group">
                         <div className="p-2 border border-slate-500 rounded-md hover:border-slate-200 transition">
-                            <Image
-                                src="/whatsapp.png"
-                                alt="github"
-                                width={24} height={24} />
+                            <Image src="/whatsapp.png" alt="whatsapp" width={24} height={24} />
                         </div>
                         <span className="absolute -top-8 left-1/2 -translate-x-1/2 bg-slate-200 text-black text-sm px-2 py-1 rounded-md opacity-0 group-hover:opacity-100 transition">
                             Whatsapp
                         </span>
                     </a>
-
                 </div>
             </div>
         </div>
