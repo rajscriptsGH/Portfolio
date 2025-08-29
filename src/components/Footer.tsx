3c238776e59cfd0a183ed4292e51da9d21054a90
rajyadav63 <vector36razz@gmail.com>
2025-08-29 12:29:00 +0530

    eslint error resolved

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,18 @@
 "use client"
 import { useEffect, useState } from "react"
 
+const gitaQuotes: string[] = [
+    "You have the right to work, but never to the fruit of work. — BG 2.47",
+    "Change is the law of the universe. You can be a millionaire, or a pauper in an instant. — BG 2.14",
+    "A person can rise through the efforts of his own mind; or draw himself down, in the same manner. — BG 6.5",
+    "Perform your duty without attachment, for action performed without desire brings the supreme. — BG 3.19",
+    "For one who has conquered the mind, the mind is the best friend; but for one who has failed, it is the worst enemy. — BG 6.6",
+    "The soul is neither born, and nor does it die. — BG 2.20",
+    "Whatever happened, happened for the good. Whatever is happening, is happening for the good. Whatever will happen, will also happen for the good. — BG 4.18",
+]
+
 const Footer = () => {
     const [quote, setQuote] = useState<string>("")
-
-    const gitaQuotes: string[] = [
-        "You have the right to work, but never to the fruit of work. — BG 2.47",
-        "Change is the law of the universe. You can be a millionaire, or a pauper in an instant. — BG 2.14",
-        "A person can rise through the efforts of his own mind; or draw himself down, in the same manner. — BG 6.5",
-        "Perform your duty without attachment, for action performed without desire brings the supreme. — BG 3.19",
-        "For one who has conquered the mind, the mind is the best friend; but for one who has failed, it is the worst enemy. — BG 6.6",
-        "The soul is neither born, and nor does it die. — BG 2.20",
-        "Whatever happened, happened for the good. Whatever is happening, is happening for the good. Whatever will happen, will also happen for the good. — BG 4.18",
-    ]
 
     useEffect(() => {
         const now = new Date()
