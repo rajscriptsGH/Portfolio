8b476a6c082ff91a491b39a931ef5d992e82938d
rajyadav63 <vector36razz@gmail.com>
2025-08-31 17:44:34 +0530

    bytebrain project added

--- src/components/FeaturedProjects.tsx
+++ src/components/FeaturedProjects.tsx
@@ -34,7 +34,7 @@ const FeaturedProjects = () => {
 
                         {/* Tech Stack */}
                         <div className="flex flex-wrap gap-2 mb-4">
-                            {["React", "Node.js", "MongoDB", "Express"].map((tech, key) => (
+                            {["React", "Node.js", "MongoDB", "Express", "Tailwind"].map((tech, key) => (
                                 <span
                                     key={key}
                                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
@@ -66,12 +66,12 @@ const FeaturedProjects = () => {
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
 
                         {/* Title */}
-                        <h3 className="text-xl font-bold mb-3">Pictofy</h3>
+                        <h3 className="text-xl font-bold mb-3">ByteBrain</h3>
 
                         {/* Image */}
                         <div className="relative w-full h-60 overflow-hidden rounded-2xl">
                             <Image
-                                src="/pictofy.png"
+                                src="/bytebrain.png"
                                 alt="pictofy"
                                 fill
                                 className="object-cover rounded-2xl transition-all duration-700 hover:scale-110"
@@ -81,12 +81,12 @@ const FeaturedProjects = () => {
 
                         {/* Description */}
                         <p className="text-gray-400 mt-4 mb-4">
-                            A modern AI-powered web app that generates stunning images from text.
+                            A full-stack Second Brain application designed to help individuals to store their thoughts, ideas, and notes in a structured way
                         </p>
 
                         {/* Tech Stack */}
                         <div className="flex flex-wrap gap-2 mb-4">
-                            {["React", "Node.js", "MongoDB", "Express"].map((tech, key) => (
+                            {["TypeScript", "React", "Node.js", "MongoDB", "Express", "Tailwind"].map((tech, key) => (
                                 <span
                                     key={key}
                                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
@@ -99,7 +99,7 @@ const FeaturedProjects = () => {
                         {/* Buttons */}
                         <div className="flex gap-3">
                             <a
-                                href="https://github.com/rajscriptsGH/Pictofy"
+                                href="https://github.com/rajscriptsGH/ByteBrain"
                                 target="_blank"
                                 className="px-4 py-1 rounded-lg border border-blue-500 text-white hover:bg-slate-300 hover:text-black transition-colors"
                             >
