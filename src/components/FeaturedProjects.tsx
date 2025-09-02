19641c535d57631d30b09a322551163f812916f5
rajyadav63 <vector36razz@gmail.com>
2025-09-02 20:52:30 +0530

    anonquest and talksy project added

--- src/components/FeaturedProjects.tsx
+++ src/components/FeaturedProjects.tsx
@@ -118,12 +118,12 @@ const FeaturedProjects = () => {
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
 
                         {/* Title */}
-                        <h3 className="text-xl font-bold mb-3">Pictofy</h3>
+                        <h3 className="text-xl font-bold mb-3">AnonQuest <span className='text-[15px] font-extralight ml-3 text-gray-400'>in progress</span></h3>
 
                         {/* Image */}
                         <div className="relative w-full h-60 overflow-hidden rounded-2xl">
                             <Image
-                                src="/pictofy.png"
+                                src="/anonquest.png"
                                 alt="pictofy"
                                 fill
                                 className="object-cover rounded-2xl transition-all duration-700 hover:scale-110"
@@ -133,12 +133,12 @@ const FeaturedProjects = () => {
 
                         {/* Description */}
                         <p className="text-gray-400 mt-4 mb-4">
-                            A modern AI-powered web app that generates stunning images from text.
+                            AnonQuest is an open-source AMA (Ask Me Anything) web app that allows users to ask and answer questions anonymously.
                         </p>
 
                         {/* Tech Stack */}
                         <div className="flex flex-wrap gap-2 mb-4">
-                            {["React", "Node.js", "MongoDB", "Express"].map((tech, key) => (
+                            {["Next.js", "TypeScript", "Tailwind", "Node.js", "MongoDB", "Express"].map((tech, key) => (
                                 <span
                                     key={key}
                                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
@@ -151,14 +151,14 @@ const FeaturedProjects = () => {
                         {/* Buttons */}
                         <div className="flex gap-3">
                             <a
-                                href="https://github.com/rajscriptsGH/Pictofy"
+                                href="https://github.com/rajscriptsGH/AnonQuest"
                                 target="_blank"
                                 className="px-4 py-1 rounded-lg border border-blue-500 text-white hover:bg-slate-300 hover:text-black transition-colors"
                             >
                                 GitHub
                             </a>
                             <a
-                                href="https://pictofy.vercel.app"
+                                href="https://AnonQuest.vercel.app"
                                 target="_blank"
                                 className="px-4 py-1 rounded-lg bg-green-700 text-white hover:bg-orange-400 transition-colors"
                             >
@@ -170,12 +170,12 @@ const FeaturedProjects = () => {
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
 
                         {/* Title */}
-                        <h3 className="text-xl font-bold mb-3">Pictofy</h3>
+                        <h3 className="text-xl font-bold mb-3">Talksy <span className='text-[15px] font-extralight ml-3 text-gray-400'>in progress</span></h3>
 
                         {/* Image */}
                         <div className="relative w-full h-60 overflow-hidden rounded-2xl">
                             <Image
-                                src="/pictofy.png"
+                                src="/talksyChat.png"
                                 alt="pictofy"
                                 fill
                                 className="object-cover rounded-2xl transition-all duration-700 hover:scale-110"
@@ -185,12 +185,12 @@ const FeaturedProjects = () => {
 
                         {/* Description */}
                         <p className="text-gray-400 mt-4 mb-4">
-                            A modern AI-powered web app that generates stunning images from text.
+                            Talksy - A real-time chat and video app for fast and simple communication
                         </p>
 
                         {/* Tech Stack */}
                         <div className="flex flex-wrap gap-2 mb-4">
-                            {["React", "Node.js", "MongoDB", "Express"].map((tech, key) => (
+                            {["React", "Node.js", "MongoDB", "Express", "Tailwind", "Socket.io", "Stream"].map((tech, key) => (
                                 <span
                                     key={key}
                                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
@@ -203,14 +203,14 @@ const FeaturedProjects = () => {
                         {/* Buttons */}
                         <div className="flex gap-3">
                             <a
-                                href="https://github.com/rajscriptsGH/Pictofy"
+                                href="https://github.com/rajscriptsGH/Talksy"
                                 target="_blank"
                                 className="px-4 py-1 rounded-lg border border-blue-500 text-white hover:bg-slate-300 hover:text-black transition-colors"
                             >
                                 GitHub
                             </a>
                             <a
-                                href="https://pictofy.vercel.app"
+                                href="https://talksy.vercel.app"
                                 target="_blank"
                                 className="px-4 py-1 rounded-lg bg-green-700 text-white hover:bg-orange-400 transition-colors"
                             >
