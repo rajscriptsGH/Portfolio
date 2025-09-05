7e699e070dda93fa577e758345217a08293c4d30
rajyadav63 <vector36razz@gmail.com>
2025-09-05 20:50:54 +0530

    theme change for home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,9 @@ const Navbar = () => {
         <div>
 
             <nav className='flex justify-center items-center w-full py-3 px-16 relative z-10 mt-20'>
-                <ul className='flex items-center py-3 rounded-lg px-4 gap-2 lg:gap-10 bg-slate-600/90 shadow-sm'>
+                <ul className='fixed  flex items-center py-3 rounded-lg px-4 gap-2 lg:gap-10 bg-slate-600/90 shadow-sm'>
                     <li>
-                        <a className='px-2 hover:text-slate-300 font-sans' href="#home">Home</a>
+                        <a className='px-2 hover:text-slate-300 font-sans' href="/">Home</a>
                     </li>
                     <li>
                         <a className='px-2 hover:text-slate-300 font-sans' href="#project">
