4373f1cc95ffc3dc663299fc093e37ebd3951017
rajyadav63 <vector36razz@gmail.com>
2025-09-18 18:25:52 +0530

    image size and its priority fixed

--- src/components/FeaturedProjects.tsx
+++ src/components/FeaturedProjects.tsx
@@ -60,6 +60,8 @@ const FeaturedProjects = () => {
                                 src={p.img}
                                 alt={p.title}
                                 fill
+                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                priority
                                 className="object-cover transform transition-transform duration-700 group-hover:scale-105"
                             />
 
