f01d07f315e18f5d10db08c8e4912b71ca4c74c8
rajyadav63 <vector36razz@gmail.com>
2025-12-25 20:05:11 +0545

    contact section improved

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,24 +1,35 @@
-'use client'
-import React from 'react'
+"use client"
+import React from "react"
 
 const Contact = () => {
     return (
-        <div className="min-h-[60vh] flex flex-col items-center justify-center py-12 px-4">
-            <h2 className="text-3xl font-bold mb-6">Contact Me</h2>
+        <section className="min-h-[35vh] flex flex-col items-center justify-center py-10 px-2 lg:ml-24 bg-gray-900 text-white rounded-2xl">
+            <h2 className="text-3xl font-bold mb-4">Let's Work Together</h2>
 
-            <p className="text-gray-400 mb-8 text-center max-w-xl">
-                Book a free 15-minute session to bring clarity to your vision and kickstart our journey together.
+            <p className="text-gray-400 mb-8 text-center max-w-2xl">
+                I’m open to collaborating on innovative projects and technology-driven initiatives. If you have a proposal or would like to discuss potential opportunities, feel free to get in touch!
             </p>
 
-            <a
-                href="https://cal.com/rajscripts/15min"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="px-6 py-3 rounded-xl bg-blue-500 text-white font-semibold hover:bg-blue-600 transition"
-            >
-                Book a Call →
-            </a>
-        </div>
+            <div className="flex flex-col sm:flex-row gap-4 mb-6">
+                <a
+                    href="https://cal.com/rajscripts/15min"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-2 px-6 py-3 rounded-xl bg-gray-800 border border-gray-700 text-white font-semibold hover:bg-gray-700 transition shadow-sm hover:shadow-md"
+                >
+                    <img src="/profile.jpg" alt="Cal" className="w-5 h-5  rounded-2xl" />
+                    Book a Call
+                </a>
+
+                <a
+                    href="mailto:vector36razz@gmail.com"
+                    className="flex items-center gap-2 px-6 py-3 rounded-xl bg-gray-800 border border-gray-700 text-white font-semibold hover:bg-gray-700 transition shadow-sm hover:shadow-md"
+                >
+                    <img src="/gmail.png" alt="Mail" className="w-5 h-5 rounded-2xl" />
+                    Send Message
+                </a>
+            </div>
+        </section>
     )
 }
 
